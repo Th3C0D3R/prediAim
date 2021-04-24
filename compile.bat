6d5b835826242e3afdd914ece60c13c6c4969088
Th3OnlyC0D3R <Th3C0D3R@users.noreply.github.com>
2021-04-24 06:02:52 +0200

    now removed unused models

--- compile.bat
+++ compile.bat
@@ -1,5 +1,14 @@
 del mod_predictAim.pyc
 python -m py_compile mod_predictAim.py
-xcopy /F /Y "mod_predictAim.pyc" "C:\Games\World_of_Tanks_EU\res_mods\1.12.1.0\scripts\client\gui\mods\mod_predictAim.pyc"
-xcopy /S /Y "objects" "C:\Games\World_of_Tanks_EU\res_mods\mods\shared_resources\xvm\res\objects\"
+
+REM !! change the path relating to your location !!
+
+REM xcopy /F /Y "mod_predictAim.pyc" "C:\Games\World_of_Tanks_EU\res_mods\1.12.1.0\scripts\client\gui\mods\mod_predictAim.pyc"
+
+
+
+REM !! you should change this path as well but it needs to stay at the location so only change the stuff before \res_mods !!
+
+REM xcopy /S /Y "objects" "C:\Games\World_of_Tanks_EU\res_mods\mods\shared_resources\xvm\res\objects\"
+
 PAUSE
